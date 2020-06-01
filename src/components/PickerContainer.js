adfb34a51f984a52b19299c13d88c09e5a999a8f
Ishaan Dixit <31419120+idixit100@users.noreply.github.com>
2020-05-31 23:06:55 -0400

    Add a picker container
    Purpose is to be a wrapper that will be skin to the picker panel

---
+++ src/components/PickerContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/PickerContainer.css";
+import LunchPickerPanel from "./LunchPickerPanel";
+
+function PickerContainer() {
+  return (
+    <div class="PickerContainer">
+      <LunchPickerPanel />
+    </div>
+  );
+}
+
+export default PickerContainer;
