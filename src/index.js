ecde942fb8dbe59701508d3e5766d04611731739
Ishaan Dixit <31419120+idixit100@users.noreply.github.com>
2020-05-31 21:32:50 -0400

    Add base.css to get rid of those pesky margins

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import "./css/base.css";
 import App from "./components/App";
 
 const rootElement = document.getElementById("root");
